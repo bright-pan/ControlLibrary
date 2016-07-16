57fb5f84059f9a4ef8ff38fee289f4a7406a4a7b
droma94 <diogomiguel0@hotmail.com>
2016-07-17 00:57:50 +0100

    Fixed the weird referencing method previously used.

--- control.c
+++ control.c
@@ -1,7 +1,14 @@
 #include "control.h"
 
+/*
+*	Proportional and Integrator Controller:
+*		K: Proportional constant
+*		K_i: Integrator constant
+*		u:
+*/
+
 void PI(float K, float K_i, float* u, float* u_i, float err, float h)
 {
-	u_i[0] = K*u_i[0] + h*K_i*err;
-	u[0] = u_i[0] + K*err;
+	*u_i = K*(*u_i) + h*K_i*err;
+	*u = *u_i + K*err;
 }
