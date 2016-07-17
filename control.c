fbd919aad52e4e61645668cf2b2bc50f6a372881
droma94 <diogomiguel0@hotmail.com>
2016-07-17 01:09:28 +0100

    Forgot to finish the intro comments

--- control.c
+++ control.c
@@ -4,7 +4,10 @@
 *	Proportional and Integrator Controller:
 *		K: Proportional constant
 *		K_i: Integrator constant
-*		u:
+*		u: Full output of Controller. Must be defined as zero in the main 		funcion
+*		u_i: Partial output, only relative to the integrator. Same constriction the variable above
+*		err: Error funcion
+*		h: Sample time in seconds
 */
 
 void PI(float K, float K_i, float* u, float* u_i, float err, float h)
