1c8ff3adb485bc189a1bb2d1607cdf878797e26b
droma94 <diogomiguel0@hotmail.com>
2016-07-17 14:00:02 +0100

    Forgot to fix capitalization

--- control.h
+++ control.h
@@ -1,7 +1,7 @@
 #ifndef CONTROL_H_
 #define CONTROL_H_
 
-void PI(float K, float K_i, float* u, float* u_i, float err, float h);
+void PI(float k, float k_i, float* u, float* u_i, float err, float h);
 void PID(float k_p, float k_i, float k_d, float* u, float err0, float err1, float err2, float h);
 
 #endif
